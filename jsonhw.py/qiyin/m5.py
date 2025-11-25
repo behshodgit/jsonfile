94064a47564b08b1e143aeebdc4d8cdb328a6e68
behshod <behshod@example.com>
2025-11-25 15:26:31 +0500

    ozgartirdim

--- jsonhw.py/qiyin/m5.py
+++ jsonhw.py/qiyin/m5.py
@@ -5,4 +5,4 @@ with open("social_users.json",'r',encoding="utf-8") as f:
 social_users=data.get("users")
 l=list(filter(lambda x:len(x["social"])>=2,social_users))
 name=[i["name"]for i in l]
-print(name)
+print(name )
